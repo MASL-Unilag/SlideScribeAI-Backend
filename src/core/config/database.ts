df3ce4776920c01db093b9d51c9cb23166f6492b
tdadadavid <davidtofunmidada@gmail.com>
2023-11-17 14:43:15 +0100

    add pipeline for streamlined dev & deploy

--- src/core/config/database.ts
+++ src/core/config/database.ts
@@ -3,6 +3,7 @@ import config from "./config";
 
 // database connection.
 const initializeDbConnection = async () => {
+  console.log(config);
   return mongoose.connect(config.db.mongodb.MONGO_URL);
 };
 
